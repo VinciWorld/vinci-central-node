ae33ba11cb32664904b9711ae5f66c3abd391b08
Jorge Costa <jorgemdcosta@gmail.com>
2023-10-03 11:33:14 +0100

    Update train job schema

--- app/domains/train_job/schemas/train_job.py
+++ app/domains/train_job/schemas/train_job.py
@@ -7,23 +7,32 @@ from app.domains.core.schemas.user import UserSchema
 from app.domains.train_job.schemas.train_job_constants import TrainJobStatus, TrainJobType
 
 
+class EnvConfig(BaseModel):
+    env_id: str
+    num_of_areas: int
+
+class NnModelConfig(BaseModel):
+    behavior_name: str
+    steps: int
+
+
 class TrainJobQueue(BaseModel):
     centra_node_id: uuid.UUID
     central_node_url: str
     run_id: uuid.UUID
     job_type: TrainJobType
-    nn_model_config: str
+    nn_model_config: NnModelConfig
     agent_config: str
-    env_config: str
+    env_config: EnvConfig
 
 
 class TrainJobsBase(BaseModel):
     run_id: uuid.UUID
     job_status: TrainJobStatus
     job_type: TrainJobType
-    nn_model_config: str
+    nn_model_config: NnModelConfig
     agent_config: str
-    env_config: str
+    env_config: EnvConfig
 
 
 class TrainJobCreate(TrainJobsBase):
@@ -40,11 +49,11 @@ class TrainJobSchema(TrainJobsBase):
 
 
 class TrainJobResponseSchema(TrainJobSchema):
-    pass
+    train_node_url:str
 
 
 class TrainJobBody(BaseModel):
     run_id: Optional[uuid.UUID] | None = None
     agent_config: str
-    env_config: str
-    nn_model_config: str
+    nn_model_config: NnModelConfig
+    env_config: EnvConfig
