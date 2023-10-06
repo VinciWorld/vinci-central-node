9b6f2fd449aa0b1b94b436cba7abe61150c737c6
Pedro Marques <pedrommarquesm@gmail.com>
2023-10-06 16:18:15 +0100

    cloudwatch logging

--- app/main.py
+++ app/main.py
@@ -7,18 +7,22 @@ from app.settings.settings import settings
 from app.domains.train_job.controller.train_job import train_job_router
 from app.domains.train_results.controllers.train_results import train_results_router
 from app.domains.train_stream.controllers.train_stream import train_stream_router
-
+from app.logs.CloudWatchLogHandler import setup_cloudwatch_logging
 import logging
 
-logging.config.fileConfig('logging.conf', disable_existing_loggers=False)  #type: ignore
-logger = logging.getLogger(__name__) 
+log_group_name = "vinci-world-cloud-dev-CentralNodeLogGroup"
 
+logging.config.fileConfig('logging.conf', disable_existing_loggers=False)  #type: ignore
+
+logging.info('Setting up cloudwatch logging')
+setup_cloudwatch_logging(log_group_name)
+
+logger = logging.getLogger(__name__) 
 
 app = FastAPI()
 app.include_router(train_job_router)
 app.include_router(train_stream_router)
 app.include_router(train_results_router)
-
 
 
 @app.on_event("startup")
