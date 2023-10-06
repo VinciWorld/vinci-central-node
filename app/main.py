dec916a2d78373f76b58035f1f625889787a24fe
Jorge Costa <jorgemdcosta@gmail.com>
2023-10-06 17:43:14 +0100

    Add Cors

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from app.db.connection import Session
 from app.domains.core.models.user import ensure_default_user
 from alembic.config import Config
 from alembic.command import upgrade
+from fastapi.middleware.cors import CORSMiddleware
 from app.settings.settings import settings
 from app.domains.train_job.controller.train_job import train_job_router
 from app.domains.train_results.controllers.train_results import train_results_router
@@ -23,6 +24,15 @@ app = FastAPI()
 app.include_router(train_job_router)
 app.include_router(train_stream_router)
 app.include_router(train_results_router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+
 
 
 @app.on_event("startup")
